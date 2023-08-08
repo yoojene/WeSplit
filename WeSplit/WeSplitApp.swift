1cf30f7485035e36de9651de01736e2d54e710a0
Eugene Cross <eugene.cross@gmail.com>
2023-08-08 13:46:13 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Eugene on 08/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
